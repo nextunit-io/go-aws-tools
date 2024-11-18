de1e00a81e2bf60a53d506eaeee0a8b0f4f7387d
Nils B. <zero+github@nextunit.io>
2024-11-18 11:22:28 +0100

    Add presigner mock

---
+++ awstoolsmock/presignermock.go
@@ -0,0 +1,144 @@
+package awstoolsmock
+
+import (
+	"context"
+	"fmt"
+
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	gomock "github.com/nextunit-io/go-mock"
+)
+
+type presignMockStruct struct {
+	PresignGetObject *gomock.ToolMock[struct {
+		Ctx    context.Context
+		Params *s3.GetObjectInput
+		OptFns []func(*s3.PresignOptions)
+	},
+		v4.PresignedHTTPRequest,
+	]
+	PresignDeleteObject *gomock.ToolMock[struct {
+		Ctx    context.Context
+		Params *s3.DeleteObjectInput
+		OptFns []func(*s3.PresignOptions)
+	},
+		v4.PresignedHTTPRequest,
+	]
+	PresignPutObject *gomock.ToolMock[struct {
+		Ctx    context.Context
+		Params *s3.PutObjectInput
+		OptFns []func(*s3.PresignOptions)
+	},
+		v4.PresignedHTTPRequest,
+	]
+	PresignPostObject *gomock.ToolMock[struct {
+		Ctx    context.Context
+		Params *s3.PutObjectInput
+		OptFns []func(*s3.PresignOptions)
+	},
+		v4.PresignedHTTPRequest,
+	]
+}
+
+type PresignMock struct {
+	Mock presignMockStruct
+}
+
+func GetPresignerMock() *PresignMock {
+	return &PresignMock{
+		Mock: presignMockStruct{
+			PresignGetObject: gomock.GetMock[struct {
+				Ctx    context.Context
+				Params *s3.GetObjectInput
+				OptFns []func(*s3.PresignOptions)
+			},
+				v4.PresignedHTTPRequest,
+			](fmt.Errorf("PresignGetObject general error")),
+			PresignDeleteObject: gomock.GetMock[struct {
+				Ctx    context.Context
+				Params *s3.DeleteObjectInput
+				OptFns []func(*s3.PresignOptions)
+			},
+				v4.PresignedHTTPRequest,
+			](fmt.Errorf("PresignDeleteObject general error")),
+			PresignPutObject: gomock.GetMock[struct {
+				Ctx    context.Context
+				Params *s3.PutObjectInput
+				OptFns []func(*s3.PresignOptions)
+			},
+				v4.PresignedHTTPRequest,
+			](fmt.Errorf("PresignPutObject general error")),
+			PresignPostObject: gomock.GetMock[struct {
+				Ctx    context.Context
+				Params *s3.PutObjectInput
+				OptFns []func(*s3.PresignOptions)
+			},
+				v4.PresignedHTTPRequest,
+			](fmt.Errorf("PresignPostObject general error")),
+		},
+	}
+}
+
+func (p *PresignMock) PresignGetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.PresignOptions)) (*v4.PresignedHTTPRequest, error) {
+	p.Mock.PresignGetObject.AddInput(
+		struct {
+			Ctx    context.Context
+			Params *s3.GetObjectInput
+			OptFns []func(*s3.PresignOptions)
+		}{
+			Ctx:    ctx,
+			Params: params,
+			OptFns: optFns,
+		},
+	)
+
+	return p.Mock.PresignGetObject.GetNextResult()
+}
+
+func (p *PresignMock) PresignDeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.PresignOptions)) (*v4.PresignedHTTPRequest, error) {
+	p.Mock.PresignDeleteObject.AddInput(
+		struct {
+			Ctx    context.Context
+			Params *s3.DeleteObjectInput
+			OptFns []func(*s3.PresignOptions)
+		}{
+			Ctx:    ctx,
+			Params: params,
+			OptFns: optFns,
+		},
+	)
+
+	return p.Mock.PresignDeleteObject.GetNextResult()
+}
+
+func (p *PresignMock) PresignPutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.PresignOptions)) (*v4.PresignedHTTPRequest, error) {
+	p.Mock.PresignPutObject.AddInput(
+		struct {
+			Ctx    context.Context
+			Params *s3.PutObjectInput
+			OptFns []func(*s3.PresignOptions)
+		}{
+			Ctx:    ctx,
+			Params: params,
+			OptFns: optFns,
+		},
+	)
+
+	return p.Mock.PresignPutObject.GetNextResult()
+}
+
+func (p *PresignMock) PresignPostObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.PresignOptions)) (*v4.PresignedHTTPRequest, error) {
+	p.Mock.PresignPostObject.AddInput(
+		struct {
+			Ctx    context.Context
+			Params *s3.PutObjectInput
+			OptFns []func(*s3.PresignOptions)
+		}{
+			Ctx:    ctx,
+			Params: params,
+			OptFns: optFns,
+		},
+	)
+
+	return p.Mock.PresignPostObject.GetNextResult()
+}
---
+++ awstoolsmock/presignermock_test.go
@@ -0,0 +1,201 @@
+package awstoolsmock_test
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/nextunit-io/go-aws-tools/awstoolsmock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPresignerMockPresignGetObject(t *testing.T) {
+	t.Helper()
+	presignerMock := awstoolsmock.GetPresignerMock()
+	ctx := context.TODO()
+
+	t.Run("Testing PresignGetObject", func(t *testing.T) {
+		output := v4.PresignedHTTPRequest{
+			URL: "test-url-123",
+		}
+
+		presignerMock.Mock.PresignGetObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignGetObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignGetObject.AddReturnValue(&output)
+
+		for i := 0; i < 3; i++ {
+			o, err := presignerMock.PresignGetObject(ctx, &s3.GetObjectInput{
+				Bucket: aws.String("test-bucket"),
+				Key:    aws.String(fmt.Sprintf("file-%d", i)),
+			})
+
+			assert.Nil(t, err)
+			assert.Equal(t, output, *o)
+		}
+
+		o, err := presignerMock.PresignGetObject(ctx, &s3.GetObjectInput{
+			Bucket: aws.String("test-bucket"),
+			Key:    aws.String("error"),
+		})
+
+		assert.Nil(t, o)
+		assert.Equal(t, fmt.Errorf("PresignGetObject general error"), err)
+
+		assert.Equal(t, 4, presignerMock.Mock.PresignGetObject.HasBeenCalled())
+		for i := 0; i < 3; i++ {
+			input := presignerMock.Mock.PresignGetObject.GetInput(i)
+			assert.Equal(t, ctx, input.Ctx)
+			assert.Equal(t, "test-bucket", *input.Params.Bucket)
+			assert.Equal(t, fmt.Sprintf("file-%d", i), *input.Params.Key)
+		}
+
+		input := presignerMock.Mock.PresignGetObject.GetInput(3)
+		assert.Equal(t, ctx, input.Ctx)
+		assert.Equal(t, "test-bucket", *input.Params.Bucket)
+		assert.Equal(t, "error", *input.Params.Key)
+	})
+}
+
+func TestPresignerMockPresignDeleteObject(t *testing.T) {
+	t.Helper()
+	presignerMock := awstoolsmock.GetPresignerMock()
+	ctx := context.TODO()
+
+	t.Run("Testing PresignDeleteObject", func(t *testing.T) {
+		output := v4.PresignedHTTPRequest{
+			URL: "test-url-123",
+		}
+
+		presignerMock.Mock.PresignDeleteObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignDeleteObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignDeleteObject.AddReturnValue(&output)
+
+		for i := 0; i < 3; i++ {
+			o, err := presignerMock.PresignDeleteObject(ctx, &s3.DeleteObjectInput{
+				Bucket: aws.String("test-bucket"),
+				Key:    aws.String(fmt.Sprintf("file-%d", i)),
+			})
+
+			assert.Nil(t, err)
+			assert.Equal(t, output, *o)
+		}
+
+		o, err := presignerMock.PresignDeleteObject(ctx, &s3.DeleteObjectInput{
+			Bucket: aws.String("test-bucket"),
+			Key:    aws.String("error"),
+		})
+
+		assert.Nil(t, o)
+		assert.Equal(t, fmt.Errorf("PresignDeleteObject general error"), err)
+
+		assert.Equal(t, 4, presignerMock.Mock.PresignDeleteObject.HasBeenCalled())
+		for i := 0; i < 3; i++ {
+			input := presignerMock.Mock.PresignDeleteObject.GetInput(i)
+			assert.Equal(t, ctx, input.Ctx)
+			assert.Equal(t, "test-bucket", *input.Params.Bucket)
+			assert.Equal(t, fmt.Sprintf("file-%d", i), *input.Params.Key)
+		}
+
+		input := presignerMock.Mock.PresignDeleteObject.GetInput(3)
+		assert.Equal(t, ctx, input.Ctx)
+		assert.Equal(t, "test-bucket", *input.Params.Bucket)
+		assert.Equal(t, "error", *input.Params.Key)
+	})
+}
+
+func TestPresignerMockPresignPutObject(t *testing.T) {
+	t.Helper()
+	presignerMock := awstoolsmock.GetPresignerMock()
+	ctx := context.TODO()
+
+	t.Run("Testing PresignPutObject", func(t *testing.T) {
+		output := v4.PresignedHTTPRequest{
+			URL: "test-url-123",
+		}
+
+		presignerMock.Mock.PresignPutObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignPutObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignPutObject.AddReturnValue(&output)
+
+		for i := 0; i < 3; i++ {
+			o, err := presignerMock.PresignPutObject(ctx, &s3.PutObjectInput{
+				Bucket: aws.String("test-bucket"),
+				Key:    aws.String(fmt.Sprintf("file-%d", i)),
+			})
+
+			assert.Nil(t, err)
+			assert.Equal(t, output, *o)
+		}
+
+		o, err := presignerMock.PresignPutObject(ctx, &s3.PutObjectInput{
+			Bucket: aws.String("test-bucket"),
+			Key:    aws.String("error"),
+		})
+
+		assert.Nil(t, o)
+		assert.Equal(t, fmt.Errorf("PresignPutObject general error"), err)
+
+		assert.Equal(t, 4, presignerMock.Mock.PresignPutObject.HasBeenCalled())
+		for i := 0; i < 3; i++ {
+			input := presignerMock.Mock.PresignPutObject.GetInput(i)
+			assert.Equal(t, ctx, input.Ctx)
+			assert.Equal(t, "test-bucket", *input.Params.Bucket)
+			assert.Equal(t, fmt.Sprintf("file-%d", i), *input.Params.Key)
+		}
+
+		input := presignerMock.Mock.PresignPutObject.GetInput(3)
+		assert.Equal(t, ctx, input.Ctx)
+		assert.Equal(t, "test-bucket", *input.Params.Bucket)
+		assert.Equal(t, "error", *input.Params.Key)
+	})
+}
+
+func TestPresignerMockPresignPostObject(t *testing.T) {
+	t.Helper()
+	presignerMock := awstoolsmock.GetPresignerMock()
+	ctx := context.TODO()
+
+	t.Run("Testing PresignPostObject", func(t *testing.T) {
+		output := v4.PresignedHTTPRequest{
+			URL: "test-url-123",
+		}
+
+		presignerMock.Mock.PresignPostObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignPostObject.AddReturnValue(&output)
+		presignerMock.Mock.PresignPostObject.AddReturnValue(&output)
+
+		for i := 0; i < 3; i++ {
+			o, err := presignerMock.PresignPostObject(ctx, &s3.PutObjectInput{
+				Bucket: aws.String("test-bucket"),
+				Key:    aws.String(fmt.Sprintf("file-%d", i)),
+			})
+
+			assert.Nil(t, err)
+			assert.Equal(t, output, *o)
+		}
+
+		o, err := presignerMock.PresignPostObject(ctx, &s3.PutObjectInput{
+			Bucket: aws.String("test-bucket"),
+			Key:    aws.String("error"),
+		})
+
+		assert.Nil(t, o)
+		assert.Equal(t, fmt.Errorf("PresignPostObject general error"), err)
+
+		assert.Equal(t, 4, presignerMock.Mock.PresignPostObject.HasBeenCalled())
+		for i := 0; i < 3; i++ {
+			input := presignerMock.Mock.PresignPostObject.GetInput(i)
+			assert.Equal(t, ctx, input.Ctx)
+			assert.Equal(t, "test-bucket", *input.Params.Bucket)
+			assert.Equal(t, fmt.Sprintf("file-%d", i), *input.Params.Key)
+		}
+
+		input := presignerMock.Mock.PresignPostObject.GetInput(3)
+		assert.Equal(t, ctx, input.Ctx)
+		assert.Equal(t, "test-bucket", *input.Params.Bucket)
+		assert.Equal(t, "error", *input.Params.Key)
+	})
+}
