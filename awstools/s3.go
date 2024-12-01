342ac89ea7c947f6b94c8995a5cf2118bea98542
Nils B. <zero+github@nextunit.io>
2024-12-01 14:26:55 +0100

    Update awstools

--- awstools/s3.go
+++ awstools/s3.go
@@ -11,6 +11,7 @@ import (
 type S3Interface interface {
 	GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error)
 	PutObject(ctx context.Context, params *s3.PutObjectInput, optFns ...func(*s3.Options)) (*s3.PutObjectOutput, error)
+	DeleteObject(ctx context.Context, params *s3.DeleteObjectInput, optFns ...func(*s3.Options)) (*s3.DeleteObjectOutput, error)
 }
 
 var s3Instance S3Interface
